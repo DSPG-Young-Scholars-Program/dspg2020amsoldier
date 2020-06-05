be33e2d84f19679554e49d7d6d3b16bbaafcf1c8
Morgan Stockham <morganstockham@gmail.com>
2020-06-05 12:15:42 -0400

    Updated information about the df needed.

--- src/timeline_wwii.R
+++ src/timeline_wwii.R
@@ -1,1 +1,9 @@
 # WWII Timeline ------------------------------------------
+# install.packages("timelineS")
+library(timelineS)
+
+# we need to create a df with:
+# first column: event names
+# second column: dates of date class
+
+
