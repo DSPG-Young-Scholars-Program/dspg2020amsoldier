366fdd2493ffce514bbe0400ad6780f4f40d5274
Morgan <39897117+morganstockham@users.noreply.github.com>
2020-06-05 09:17:24 -0700

    Update timeline_wwii.R

--- src/timeline_wwii.R
+++ src/timeline_wwii.R
@@ -1,1 +1,7 @@
 # WWII Timeline ------------------------------------------
+# install.packages("timelineS")
+library(timelineS)
+
+# we need to create a df with:
+# first column: event names
+# second column: dates of date class
