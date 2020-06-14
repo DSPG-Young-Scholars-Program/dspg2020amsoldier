91d8e4f057f611eea2e6cffd082604d6b141440c
marysolomon <solomom@bgsu.edu>
2020-06-14 15:59:59 -0400

    try stem and lem of words

--- src/common_words.R
+++ src/common_words.R
@@ -1,5 +1,5 @@
 library(dplyr)
-library(tidytext)
+library(tidytext);library(textstem);library(SnowballC)
 library(readxl)
 
 S32W <- read_excel("~/git/Survey_32N and 32W consolidated.xlsx", sheet = 1, col_names = TRUE)
@@ -35,3 +35,57 @@ word_count77w <- word_count(S32W,T3)
 word_count78w <- word_count(S32W,T4)
 word_count78n <- word_count(S32N,T5)
 head(word_count77w);head(word_count78w);head(word_count78n)
+
+
+
+
+#--------STEMMING/LEMMATIZATION WORDS------------
+#wordStem(word_count77_W$word)
+# stemw77_W <- text77_df %>% unnest_tokens(word, text) %>% 
+#   anti_join(stop_words) %>% mutate(word= wordStem(word)) %>%
+#   count(word, sort = TRUE)
+# 
+# lemw77_W <- text77_df %>% unnest_tokens(word, text) %>% 
+#   anti_join(stop_words) %>% mutate(word= textstem::lemmatize_words(word)) %>%
+#   count(word, sort = TRUE)
+
+
+word_count_stem <- function(data, q){
+  attach(data)
+  text_df <- tibble(row = 1:nrow(data), text = q)
+  word_counts <- text_df %>% unnest_tokens(word, text) %>% 
+    anti_join(stop_words) %>% mutate(word= wordStem(word)) %>%
+    count(word, sort = TRUE)
+  detach(data)
+  return(word_counts)
+}
+word_counts77w <- word_count_stem(S32W,T3)
+word_counts78w <- word_count_stem(S32W,T4)
+word_counts78n <- word_count_stem(S32N,T5)
+head(word_counts77w);head(word_counts78w);head(word_counts78n)
+
+
+word_count_lem <- function(data, q){
+  attach(data)
+  text_df <- tibble(row = 1:nrow(data), text = q)
+  word_counts <- text_df %>% unnest_tokens(word, text) %>% 
+    anti_join(stop_words) %>% mutate(word= textstem::lemmatize_words(word)) %>%
+    count(word, sort = TRUE)
+  detach(data)
+  return(word_counts)
+}
+
+word_countl77w <- word_count_lem(S32W,T3)
+word_countl78w <- word_count_lem(S32W,T4)
+word_countl78n <- word_count_lem(S32N,T5)
+head(word_countl77w);head(word_countl78w);head(word_countl78n)
+#------STEMMING/LEMMATIZATION SENTENCES--------------
+stems77_W <- text77_df %>% mutate(text= textstem::stem_strings(text)) %>% 
+  unnest_tokens(word, text) %>% 
+  anti_join(stop_words) %>%
+  count(word, sort = TRUE)
+
+lems77_W <- text77_df %>% mutate(text= textstem::lemmatize_strings(text)) %>% 
+  unnest_tokens(word, text) %>% 
+  anti_join(stop_words)  %>%
+  count(word, sort = TRUE)
