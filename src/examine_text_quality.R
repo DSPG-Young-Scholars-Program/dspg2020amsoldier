4e6f0a3dfd8565d6ce9c560be94a68466c3a1d39
marysolomon <solomom@bgsu.edu>
2020-06-22 15:57:26 -0400

    summary tables for metadata tags

--- src/examine_text_quality.R
+++ src/examine_text_quality.R
@@ -26,33 +26,43 @@ data <- data0 %>% unite(long, long_comment:long_comment_cont, sep = " ", na.rm =
 #as character
 long <- data$long;long <- long[long != ""]
 outfit <- na.omit(data$outfits_comment)
-
+text<- c(long, outfit)
 #as data.frame
 #long <- select(data, long)
 #outfit <- na.omit(select(data, outfits_comment))
 
 ##### Look for all [unclear][/unclear] #####
 #Other metadata tags are deletion, insertion, circle and underline
-#str_extract_all(long[1:10], "\\[unclear\\]\\[\\/unclear\\]") returns metadata tags with no word in between
-long.unclear <- unlist(str_extract_all(long, "\\[unclear\\].*\\[\\/unclear\\]"))
-out.unclear <- unlist(str_extract_all(outfit, "\\[unclear\\].*\\[\\/unclear\\]"))
-#returns any use of metadata tags | drawback: if multiple of this tag are used, also returns all text in between sets of metadata tags.
+long.unclear <- unlist(str_extract_all(long, "(?=\\[unclear\\]).*?(?<=\\[\\/unclear\\])"))
+out.unclear <- unlist(str_extract_all(outfit, "(?=\\[unclear\\]).*?(?<=\\[\\/unclear\\])"))
+#returns any use of metadata tags
+long.unclear <- long.unclear[long.unclear != ""]
+out.unclear <- out.unclear[out.unclear != ""]
 
-#if data.frame
-#long <- mutate(long, unclear = str_extract_all(long, "\\[unclear\\].*\\[\\/unclear\\]"))
-
-#how to exclude character(0) from the results?
-
-long.unclear <- long.unclear[long.unclear != "character(0)"]
-out.unclear <- out.unclear[out.unclear != "character(0)"]
-#long$unclear[long$unclear == "character(0)"] <- NA #make character(0) NA..... this might help?
-#find better way to summarize results
-
-
+#perform analysis on entire collection of responses
+text.unclear <- unlist(str_extract_all(text, "(?=\\[unclear\\]).*?(?<=\\[\\/unclear\\])"))
+text.unclear <- text.unclear[text.unclear != ""]
+text.unclear <- as.data.frame(table(text.unclear))
 
 #### Which words underlined? #####
-long.underline <- unlist(str_extract_all(long, "\\[underline\\].*\\[\\/underline\\]"))
 out.underline <- unlist(str_extract_all(outfit, "\\[underline\\].*\\[\\/underline\\]"))
+long.underline <- unlist(str_extract_all(long, "(?=\\[underline\\]).*?(?<=\\[\\/underline\\])"))
+long.underline <- long.underline[long.underline != ""]
+out.underline <- out.underline[out.underline != ""]
 
-long.underline <- long.underline[long.underline != "character(0)"]
-out.underline <- out.underline[out.underline != "character(0)"]
+text.underline <- unlist(str_extract_all(text, "(?=\\[underline\\]).*?(?<=\\[\\/underline\\])"))
+text.underline <- text.underline[text.underline != ""]
+text.underline <- as.data.frame(table(text.underline))
+
+### all bracketed words that represent spell corrections ####
+text.bracket <- unlist(str_extract_all(text, "(?=\\[).*?(?<=\\])"))
+remove <- c("[unclear]","[/unclear]","[paragraph]", "[Paragraph]","[insertion]","[/insertion]", "", "[deletion]", "[/deletion]", "[underline]", "[/underline]", "[circle]", "[/circle]")
+for(i in 1:13){text.bracket <- text.bracket[text.bracket!= remove[i]]}
+text.bracket <- gsub("\\[", "", text.bracket);text.bracket <- gsub("\\]", "", text.bracket)
+text.bracket <- as.data.frame(table(text.bracket))
+
+
+##### which words circled? #####
+text.circle <- unlist(str_extract_all(text, "(?=\\[circle\\]).*?(?<=\\[\\/circle\\])"))
+text.circle <- text.circle[text.circle != ""]
+text.circle <- as.data.frame(table(text.circle))
